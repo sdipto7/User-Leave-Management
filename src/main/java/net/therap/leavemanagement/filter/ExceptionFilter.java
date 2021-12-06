e8f6787d7d1d200f9a2b6e3c037e280d9c9f80e6
Shahriar Rumi Dipto <rumi.dipto@therapservices.net>
2021-12-06 23:34:36 +0600

    Added ExceptionFilter to handle common exceptions

---
+++ src/main/java/net/therap/leavemanagement/filter/ExceptionFilter.java
@@ -0,0 +1,27 @@
+package net.therap.leavemanagement.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author rumi.dipto
+ * @since 12/6/21
+ */
+public class ExceptionFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        
+        try {
+            chain.doFilter(request, response);
+        } catch (Exception exception) {
+            httpServletRequest.setAttribute("errorMessage", exception.getMessage());
+            RequestDispatcher dispatcher = httpServletRequest.getRequestDispatcher("/error");
+            dispatcher.forward(httpServletRequest, httpServletResponse);
+        }
+    }
+}
