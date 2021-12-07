8a645fb79daf413261523fe8dc5d4742fac102c3
Shahriar Rumi Dipto <rumi.dipto@therapservices.net>
2021-12-07 10:53:34 +0600

    Added error handler method in HomeController

--- src/main/java/net/therap/leavemanagement/controller/HomeController.java
+++ src/main/java/net/therap/leavemanagement/controller/HomeController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * @author rumi.dipto
  * @since 11/21/21
@@ -14,6 +16,7 @@ public class HomeController {
 
     public static final String INDEX_PAGE = "/index";
     public static final String SUCCESS_PAGE = "/success";
+    public static final String ERROR_PAGE = "/error";
 
     @RequestMapping("/")
     public String showStartPage() {
@@ -25,5 +28,12 @@ public class HomeController {
         model.addAttribute("message", msg);
 
         return SUCCESS_PAGE;
+    }
+
+    @RequestMapping(value = "/error")
+    private String showError(HttpServletRequest request, ModelMap model) {
+        model.addAttribute("message", request.getAttribute("errorMessage"));
+
+        return ERROR_PAGE;
     }
 }
