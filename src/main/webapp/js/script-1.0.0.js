7d5d16ba50691a5c2c314cecf000b2e4ce17ba3c
Shahriar Rumi Dipto <rumi.dipto@therapservices.net>
2021-12-22 14:15:14 +0600

    Updated jquery on radio button select for leave and user save form

--- src/main/webapp/js/script-1.0.0.js
+++ src/main/webapp/js/script-1.0.0.js
@@ -1,16 +1,14 @@
 $(document).ready(function () {
-    var show = sessionStorage.getItem('show');
-    if (show === 'true') {
+    var selected = $('input[name="user.designation"]:checked').val();
+    if ((selected === 'DEVELOPER') || (selected === 'TESTER')) {
         $('#teamLeadSection').show();
     }
 
     $('input:radio[name="user.designation"]').change(function () {
         if (($(this).val() === 'DEVELOPER') || ($(this).val() === 'TESTER')) {
             $('#teamLeadSection').show();
-            sessionStorage.setItem('show', 'true');
         } else {
             $('#teamLeadSection').hide();
-            sessionStorage.setItem('show', 'false');
         }
     })
 });
@@ -22,21 +20,21 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
-    var show = sessionStorage.getItem('show');
-    if (show === 'sickLeaveCount') {
+    var selected = $('input[name="leaveType"]:checked').val();
+    if (selected === 'SICK') {
         $('#sick-leave-count-section').show();
-    } else if (show === 'casualLeaveCount') {
+        $('#casual-leave-count-section').hide();
+    } else if (selected === 'CASUAL') {
         $('#casual-leave-count-section').show();
+        $('#sick-leave-count-section').hide();
     }
 
     $('input:radio[name="leaveType"]').change(function () {
         if (($(this).val() === 'SICK')) {
             $('#sick-leave-count-section').show();
-            sessionStorage.setItem('show', 'sickLeaveCount');
             $('#casual-leave-count-section').hide();
         } else if (($(this).val() === 'CASUAL')) {
             $('#casual-leave-count-section').show();
-            sessionStorage.setItem('show', 'casualLeaveCount');
             $('#sick-leave-count-section').hide();
         }
     })
