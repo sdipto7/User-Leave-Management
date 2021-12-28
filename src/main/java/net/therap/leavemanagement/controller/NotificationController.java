c57bc500d00728814452c838f3c7813267ec035a
Shahriar Rumi Dipto <rumi.dipto@therapservices.net>
2021-12-28 13:37:27 +0600

    Removed unused method paramter from NotificationController

--- src/main/java/net/therap/leavemanagement/controller/NotificationController.java
+++ src/main/java/net/therap/leavemanagement/controller/NotificationController.java
@@ -11,8 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpSession;
-
 /**
  * @author rumi.dipto
  * @since 12/12/21
@@ -33,10 +31,7 @@ public class NotificationController {
     private UserService userService;
 
     @RequestMapping(method = RequestMethod.GET)
-    public String showNotification(@RequestParam long userId,
-                                   HttpSession session,
-                                   ModelMap model) {
-
+    public String showNotification(@RequestParam long userId, ModelMap model) {
         User user = userService.find(userId);
         authorizationHelper.checkAccess(user);
 
